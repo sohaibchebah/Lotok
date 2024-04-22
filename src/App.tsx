fea46f0a27812e9ba11a8bbbadc53f709e341eda
yacine djaaraoui <djaraoui60@gmail.com>
2024-04-22 15:37:37 +0200

    update app.css and finshing navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Home from './compenents/home'
+import "C:/Users/LENOVO/Desktop/Lotok/src/App.css"
 
 const App = () => {
   return (
--- src/compenents/navbar.tsx
+++ src/compenents/navbar.tsx
@@ -1,27 +1,31 @@
-
-import 'bootstrap/dist/css/bootstrap.css'
-import Logo from '../../public/images/logo.png'
-import '../compenents/navbar.css'
+import "bootstrap/dist/css/bootstrap.css";
+import Logo from "../../public/images/logo.png";
+import "../compenents/navbar.css";
 const Navbar = () => {
   return (
-    <nav className='navibar'> 
-      <div className='container'>
+    <nav className="navibar">
+      <div className="container">
         <div className="logo">
           <img src={Logo} alt="" />
         </div>
         <ul className="links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Brands</a></li>
-          <li><a href="#">Vehicles</a></li>
-          <li><a href="#">About</a></li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Brands</a>
+          </li>
+          <li>
+            <a href="#">Vehicles</a>
+          </li>
+          <li >
+            <a href="#">About</a>
+          </li>
           <button>log in</button>
         </ul>
       </div>
-      
     </nav>
-    
   );
-}
+};
 
-export default Navbar
-
+export default Navbar;
