3e669f1e80883bd20464d22acbd005bfea1379c2
yacine djaaraoui <djaraoui60@gmail.com>
2024-04-23 19:31:03 +0200

    normelize

--- src/compenents/navbar.tsx
+++ src/compenents/navbar.tsx
@@ -1,7 +1,7 @@
 import Logo from "../../public/images/logo.png";
 import "../compenents/navbar.css";
-import Loto from "../../public/images/rightloto.png"
-import Vector from "../../public/images/vector.jpg"
+import Loto from "../../public/images/rightloto.png";
+import Vector from "../../public/images/vector.jpg";
 const Navbar = () => {
   return (
     <>
@@ -29,26 +29,25 @@ const Navbar = () => {
       </nav>
       <div className="images">
         <div className="rouge">
-          <img src={Vector} alt=""  className="vector"/>
+          <img src={Vector} alt="" className="vector" />
         </div>
         <div className="loto">
-          <img src={Loto} alt="loto" className="loto"/>
+          <img src={Loto} alt="loto" className="loto" />
         </div>
       </div>
       <div className="texts container">
         <h1 className="rent">
-          rent 
-          your <br />
+          rent your <br />
           <span>favourite</span> <br />
           vehicle
         </h1>
         <div className="under">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusamus dolores ducimus omnis sit nobis totam pariatur optio quia voluptate.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          accusamus dolores ducimus omnis sit nobis totam pariatur optio quia
+          voluptate.
         </div>
       </div>
-      <form action="" className="reserver">
-        
-      </form>
+      <form action="" className="reserver"></form>
     </>
   );
 };
