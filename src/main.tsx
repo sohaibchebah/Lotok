8597c9aeb17ff308c0e4775d08c1c3c0add7b350
sohaib chebah <sohaib.chebah2020@gmail.com>
2024-04-28 11:20:53 +0100

    add main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./normelize.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from './compenents/Login.tsx';
+import Signup from "./compenents/Signup.tsx";
+import NotFound from "./compenents/NotFound.tsx";
+
+const router = createBrowserRouter([{
+  path: '/',
+  element : <App/>,
+  errorElement: <NotFound/>,
+},
+  {
+    path: '/Login',
+    element  : <Login/>,
+
+  }
+  ,
+  {
+    path: '/Signup',
+    element : <Signup/>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>
 );
